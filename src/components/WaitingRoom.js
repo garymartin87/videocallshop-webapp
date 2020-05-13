e19a923a96e05f57ef4931eda14c7b8f933ea882
Gary Martin <alemmartin@gmail.com>
2020-05-13 20:45:48 -0300

    Fix the socket

--- src/components/WaitingRoom.js
+++ src/components/WaitingRoom.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import socketIOClient from 'socket.io-client';
 
 import { connect } from 'react-redux';
 
 const WaitingRoom = ({ callRequest }) => {
+    const [socketConnected, setSocketConnected] = useState(false);
+    const [queue, setQueue] = useState([]);
+
     useEffect(() => {
         // ToDo: Move this to /api/callRequestSocket.js
         const socket = socketIOClient(
@@ -22,14 +25,53 @@ const WaitingRoom = ({ callRequest }) => {
                 },
             }
         );
-        socket.on('FromAPI', (data) => {
-            console.log(data);
+
+        socket.on('disconnect', () => {
+            console.log('::: SOCKET CONNECTED');
+            setSocketConnected(false);
         });
-    }, [callRequest]);
+
+        socket.on('connect', () => {
+            console.log('::: SOCKET CONNECTED');
+            setSocketConnected(true);
+        });
+
+        socket.on('WAITING_ROOM_SENDED', function (waitingRoom) {
+            console.log('::: WAITING_ROOM_SENDED', waitingRoom);
+            setQueue(waitingRoom.queue);
+        });
+
+        socket.on('QUEUE_CHANGED', function (queue) {
+            console.log('::: QUEUE_CHANGED', queue);
+            setQueue(queue);
+        });
+    }, []);
+
+    console.log('::: queue', queue, callRequest.callRequest.callRequestId);
 
     return (
         <div>
             <h1>WaitingRoom</h1>
+
+            <div style={{ textAlign: 'center' }}>
+                <h1 style={{ textAlign: 'center' }}>
+                    TIENDA ID: {callRequest.callRequest.storeId}
+                </h1>
+                <h3>{socketConnected ? 'CONNECTED' : 'DISCONNECTED'}</h3>
+            </div>
+            <hr />
+            <h4>MY callRequestId</h4>
+            <p>{callRequest.callRequest.callRequestId}</p>
+
+            <h4>Token</h4>
+            <p>{callRequest.callRequest.token}</p>
+
+            <h4>POSITION</h4>
+            <div>
+                {queue.indexOf(
+                    callRequest.callRequest.callRequestId.toString()
+                ) + 1}
+            </div>
         </div>
     );
 };
